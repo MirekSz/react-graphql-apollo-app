532c800bd93110a0495cce83937e4e1468205efb
MirekSz <miro1994>
2018-01-28 18:20:37 +0100

    delete

--- client/src/components/courses/CourseDetails.js
+++ client/src/components/courses/CourseDetails.js
@@ -7,12 +7,16 @@ class CourseDetails extends Component {
     add() {
         this.props.mutate({ variables: { input: { name: 'mirek', description: 'szajowski' } } })
     }
+    delete() {
+        this.props.delete({ variables: { id: this.props.id } });
+    }
     render() {
         const { id, course } = this.props;
         return (
             <div>
                 Details <i> {course.description}</i>
                 <button onClick={this.add.bind(this)}>Add</button>
+                <button onClick={this.delete.bind(this)}>Delete</button>
             </div>
         );
     }
@@ -26,6 +30,20 @@ query CourseQuery($id:ID!) {
     }
 }
 `, { options: ({ id }) => ({ variables: { id } }) }),
+    graphql(gql`
+    mutation Delete($id:ID!) {
+        deleteCourse(id:$id) {
+        id
+    }
+}
+`, {
+            options: ({ id }) => ({
+                variables: { id }, refetchQueries: [
+                    'CourseListQuery',
+                ]
+            }),
+            name: 'delete'
+        }),
     graphql(gql`
     mutation Add($input: CourseInput){
         createCourse(input: $input){
